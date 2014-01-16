b4fbb72f83aad7be9f4c4efd1de64c1160387ea2
NikolaevAV <nuriko@inbox.ru>
2014-01-17 02:59:05 +0400

    finish, wait fix

--- source/javascripts/application.js
+++ source/javascripts/application.js
@@ -1,2 +1,4 @@
 //= require vendor/bootstrap
+//= require vendor/jquery.form-validator
+//= require vendor/jquery.maskedinput
 //= require index
--- source/javascripts/index.js
+++ source/javascripts/index.js
@@ -1,2 +1,9 @@
-$('#myCarousel').carousel({ interval: 5000 })
-$('#feedbackCarousel').carousel({ interval: 10000 })
+jQuery(document).ready(function(){
+  $('#myCarousel').carousel({ interval: 5000 })
+  $('#feedbackCarousel').carousel({ interval: 10000 })
+
+  $.mask.definitions['~'] = "[+-]";
+  $(".iphone").mask("8 (999) 999-99-99");
+
+  $.validate();
+});
